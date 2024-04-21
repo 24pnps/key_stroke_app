dcc2041add4d267fbcfdfe9971ab604030436c6a
Ponnapassorn Iamborisut <125073493+24pnps@users.noreply.github.com>
2024-04-21 20:45:20 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:key_app/firebase_options.dart';
 import 'pages/register.dart';
 import 'pages/login.dart';
 import 'pages/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -15,7 +21,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => LoginPage(),
         '/register': (context) => RegisterPage(),
-        '/home': (context) => Home(),
+        '/home': (context) => Home(email: ""),
       },
     );
   }
